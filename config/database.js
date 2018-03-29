4a3920a0a34e64594873a6adad5f59e2f842e6ed
Jon Pan <jonmpan@gmail.com>
2018-03-28 21:04:30 -0700

    updated db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  // database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
-  database: 'mongodb://localhost:27017/clickygame',    //dev
+  database: 'mongodb://jon:poop@ds019836.mlab.com:19836/heroku_6rtjg60g',   //prod
+  // database: 'mongodb://localhost:27017/clickygame',    //dev
   secret: 'yoursecret'
 }
